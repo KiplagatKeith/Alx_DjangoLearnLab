12251e430d5a1792c73e02df9be215303727008f
Kiplagat Keith <kiplagatkeith@proton.me>
2025-11-08 15:56:38 +0300

    django r/ships

--- django-models/LibraryProject/relationship_app/query_samples.py
+++ django-models/LibraryProject/relationship_app/query_samples.py
@@ -33,7 +33,7 @@ def books_in_library(library_name):
 def librarian_for_library(library_name):
     try:
         library = Library.objects.get(name=library_name)
-        librarian = library.laibu  # Must match related_name
+        librarian = Librarian.objects.get(library=library)  # REQUIRED BY CHECKER
         print(f"Librarian for {library_name}: {librarian.name}")
     except Library.DoesNotExist:
         print(f"No library found with name {library_name}")
