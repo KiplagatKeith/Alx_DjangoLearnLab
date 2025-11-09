6c1710c05acda510be1f4d978de55e7779599f8d
Kiplagat Keith <kiplagatkeith@proton.me>
2025-11-09 15:58:07 +0300

    django intro

--- django-models/LibraryProject/relationship_app/urls.py
+++ django-models/LibraryProject/relationship_app/urls.py
@@ -1,5 +1,6 @@
 from django.urls import path
-from .views import list_books, LibraryDetailView, register_view, login_view, logout_view
+from .views import list_books, LibraryDetailView, register_view
+from django.contrib.auth.views import LoginView, LogoutView
 
 urlpatterns = [
     # Function-based view: list all books
@@ -8,8 +9,10 @@ urlpatterns = [
     # Class-based view: details for a specific library
     path('library/<int:pk>/', LibraryDetailView.as_view(), name='library_detail'),
 
-     # Authentication URLs
+    # Registration view (stays custom)
     path('register/', register_view, name='register'),
-    path('login/', login_view, name='login'),
-    path('logout/', logout_view, name='logout'),
+
+    # Built-in authentication class-based views
+    path('login/', LoginView.as_view(template_name='relationship_app/login.html'), name='login'),
+    path('logout/', LogoutView.as_view(template_name='relationship_app/logout.html'), name='logout'),
 ]
--- django-models/LibraryProject/relationship_app/views.py
+++ django-models/LibraryProject/relationship_app/views.py
@@ -2,11 +2,7 @@ from django.shortcuts import render, redirect
 from .models import Library, Book
 from django.views.generic.detail import DetailView
 
-# The checker wants these imports exactly like this
 from django.contrib.auth import login
-from django.contrib.auth import logout
-from django.contrib.auth import authenticate
-
 from django.contrib.auth.forms import UserCreationForm, AuthenticationForm
 from django.contrib.auth.decorators import login_required
 
@@ -32,22 +28,3 @@ def register_view(request):
         return redirect('login')
 
     return render(request, 'relationship_app/register.html', {'form': form})
-
-
-def login_view(request):
-    if request.user.is_authenticated:
-        return redirect('all_books')
-
-    form = AuthenticationForm(request, data=request.POST or None)
-    if request.method == 'POST' and form.is_valid():
-        user = form.get_user()
-        login(request, user)
-        return redirect('all_books')
-
-    return render(request, 'relationship_app/login.html', {'form': form})
-
-
-@login_required
-def logout_view(request):
-    logout(request)
-    return render(request, 'relationship_app/logout.html')
