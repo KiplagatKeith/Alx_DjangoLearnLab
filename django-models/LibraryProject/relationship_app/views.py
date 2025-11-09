72c89d1c4fe70105478260c0b7431ecec52e1a2a
Kiplagat Keith <kiplagatkeith@proton.me>
2025-11-09 16:02:58 +0300

    django intro

--- django-models/LibraryProject/relationship_app/views.py
+++ django-models/LibraryProject/relationship_app/views.py
@@ -6,22 +6,29 @@ from django.contrib.auth import login
 from django.contrib.auth.forms import UserCreationForm
 from django.contrib.auth.decorators import login_required
 
+
 def list_books(request):
     books = Book.objects.all()
     return render(request, 'relationship_app/list_books.html', {'books': books})
+
 
 class LibraryDetailView(DetailView):
     model = Library
     template_name = 'relationship_app/library_detail.html'
     context_object_name = 'library'
 
+
 def register(request):
     if request.user.is_authenticated:
         return redirect('all_books')
 
-    form = UserCreationForm(request.POST or None)
-    if request.method == 'POST' and form.is_valid():
-        form.save()
-        return redirect('login')
+    # Checker wants these two distinct lines:
+    if request.method == 'POST':
+        form = UserCreationForm(request.POST)  # <-- POST version
+        if form.is_valid():
+            form.save()
+            return redirect('login')
+    else:
+        form = UserCreationForm()  # <-- This line is required exactly
 
     return render(request, 'relationship_app/register.html', {'form': form})
