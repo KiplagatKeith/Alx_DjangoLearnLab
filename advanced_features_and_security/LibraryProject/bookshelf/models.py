e12c78ffb9750d945cdb74468d4a149f7bd88420
Kiplagat Keith <kiplagatkeith@proton.me>
2025-11-12 12:50:17 +0300

    advanced security and features

--- advanced_features_and_security/LibraryProject/bookshelf/models.py
+++ advanced_features_and_security/LibraryProject/bookshelf/models.py
@@ -7,5 +7,11 @@ class Book(models.Model):
     author = models.CharField(max_length = 100)
     publication_year = models.IntegerField()
 
+    class Meta:
+        permissions = [
+            ("can_create", "Can create a new book"),
+            ("can_delete", "Can delete a book"),
+        ]
+        
     def __str__(self):
         return f"Title: {self.title}, Author: {self.author}, Publication date: {self.publication_year}"
