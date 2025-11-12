73fecbc358b384b6bca540e6c681c6f4f8851f7e
Kiplagat Keith <kiplagatkeith@proton.me>
2025-11-12 13:00:03 +0300

    advanced security and features

--- advanced_features_and_security/LibraryProject/accounts/admin.py
+++
@@ -1,9 +0,0 @@
-from django.contrib import admin
-from .models import CustomUser
-
-class CustomUserModelAdmin(admin.ModelAdmin):
-    list_display = ('username', 'email', 'date_of_birth','profile_photo', 'is_staff', 'is_superuser')
-    search_fields = ('username', 'email')
-    list_filter = ('is_staff', 'is_superuser', 'is_active')
-
-admin.site.register(CustomUser, CustomUserModelAdmin)
