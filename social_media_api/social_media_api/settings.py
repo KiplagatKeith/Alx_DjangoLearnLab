ba56f808f58c08f0fe8730644f8c132feab985f0
Kiplagat Keith <kiplagatkeith@proton.me>
2025-12-12 12:18:25 +0300

    DRF TokenAuth

--- social_media_api/social_media_api/settings.py
+++ social_media_api/social_media_api/settings.py
@@ -127,3 +127,11 @@ STATIC_URL = 'static/'
 # https://docs.djangoproject.com/en/5.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+REST_FRAMEWORK = {
+    'DEFAULT_PAGINATION_CLASS': 'rest_framework.pagination.PageNumberPagination',
+    'PAGE_SIZE': 10,
+    'DEFAULT_FILTER_BACKENDS': [
+        'rest_framework.filters.SearchFilter',
+    ],
+}
