87c0cb960ceba94b47808f1ecc11d3a8a1cbd30a
Kiplagat Keith <kiplagatkeith@proton.me>
2025-12-14 22:18:29 +0300

    DRFAuth

--- social_media_api/social_media_api/settings.py
+++ social_media_api/social_media_api/settings.py
@@ -19,7 +19,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = config("DJANGO_SECRET_KEY", default=get_random_secret_key())
 
 # Disable debug in production
-DEBUG = config("DEBUG", default=False, cast=bool)
+DEBUG = False
 
 # Allowed hosts
 # Render automatically gives you a URL like your-app.onrender.com
